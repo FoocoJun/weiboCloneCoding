d8b0b0ae1f2601892db253a47853898de8d8e092
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-07-29 20:09:32 +0900

    섹션 분기점 생성

---
+++ src/components/LeftSideSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftSideSection = () => {
+  return<div>왼쪽 사이드 섹션입니다.</div>;
+};
+
+export default LeftSideSection;
---
+++ src/components/modal/SignInModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignInModal = () => {
+  return <div>로그인 모달창입니다.</div>;
+};
+
+export default SignInModal;
