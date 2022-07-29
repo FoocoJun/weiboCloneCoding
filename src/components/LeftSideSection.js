9b8465907e5ad8d2a531b107aaf3fb723599bed6
Tak2 <tinny0505@gmail.com>
2022-07-29 21:14:44 +0900

    box

--- src/components/LeftSideSection.js
+++ src/components/LeftSideSection.js
@@ -1,7 +1,59 @@
 import React from "react";
+import styled from "styled-components";
+
+
 
 const LeftSideSection = () => {
-  return<div>왼쪽 사이드 섹션입니다.</div>;
+  return (
+
+<div style={{ 
+  height: "4500px"
+
+}}>
+<Box1>
+<p>안녕</p>
+</Box1>
+
+<Box2>
+<p>안녕</p>
+</Box2>
+
+
+
+</div>
+  )
 };
+
+
+
+
+const Box1 = styled.div`
+display: inline-block;
+position: sticky;
+top: 70px;
+left: 0vw;
+
+
+width: 10vw;
+height: 100vh;
+border: 2px solid lightgray;
+
+
+
+`;
+
+const Box2 = styled.div`
+display: inline-block;
+position: sticky;
+top: 70px;
+left: 100vx;
+
+width: 10vw;
+height: 100vh;
+border: 2px solid lightgray;
+
+
+
+`;
 
 export default LeftSideSection;
