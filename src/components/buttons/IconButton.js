14b7112c0cc9767b0411a22f48f4cf221c5932cf
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-07-29 20:46:39 +0900

    DIY FontAwesome ICON

---
+++ src/components/buttons/IconButton.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCirclePlus,
+  faArrowCircleUp,
+} from "@fortawesome/free-solid-svg-icons";
+
+const IconButton = ({ string, iconName, to, size, color }) => {
+  const navigate = useNavigate();
+  const [usingIcon, setUsingIcon] = React.useState(null);
+
+  React.useEffect(() => {
+    const callImport = async () => {
+      let fontawesomeIcon = await import(`@fortawesome/free-solid-svg-icons`);
+      setUsingIcon(fontawesomeIcon[iconName]);
+    };
+    callImport();
+  }, []);
+  return (
+    <>
+    {/* icon을 불러온 이후에만 띄워준다. */}
+      {usingIcon && (
+        <div>
+          <FontAwesomeIcon
+            icon={usingIcon}
+            size={size ? size : "1x"}
+            onClick={() => {
+              navigate(to);
+            }}
+            color={color ? color : "black"}
+          />
+          {string}
+        </div>
+      )}
+    </>
+  );
+  // 사용법
+  // <IconButton string={"회원가입"} iconName={"faHome"} to={"/"} size={"1x"} color={"#fff"} />;
+};
+
+export default IconButton;
