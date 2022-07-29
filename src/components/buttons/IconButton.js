f53535056f0688ff54edd03629eb552ee023d8ce
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-07-30 00:16:31 +0900

    IconComplete

--- src/components/buttons/IconButton.js
+++ src/components/buttons/IconButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 
 const IconButton = ({ string, iconName, to, size, color }) => {
   const navigate = useNavigate();
@@ -13,21 +14,19 @@ const IconButton = ({ string, iconName, to, size, color }) => {
     };
     callImport();
   }, []);
+
   return (
     <>
-    {/* icon을 불러온 이후에만 띄워준다. */}
-      {usingIcon && (
-        <div>
-          <FontAwesomeIcon
-            icon={usingIcon}
-            size={size ? size : "1x"}
-            onClick={() => {
-              navigate(to);
-            }}
-            color={color ? color : "black"}
-          />
+      {/* icon을 불러온 이후에만 띄워준다. */}
+      {usingIcon && to &&(
+        <IconHoverBox
+          onClick={() => {
+            navigate(to);
+          }}
+        >
+          <FontAwesomeIcon icon={usingIcon} size={size ? size : "1x"} />
           {string}
-        </div>
+        </IconHoverBox>
       )}
     </>
   );
@@ -35,5 +34,17 @@ const IconButton = ({ string, iconName, to, size, color }) => {
   // <IconButton string={"회원가입"} iconName={"faHome"} to={"/"} size={"1x"} color={"#fff"} />;
 };
 
-export default IconButton;
+const IconHoverBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 38px;
+  border-radius: 4px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.15);
+    color:#ff8200;
+  }
+`;
 
+export default IconButton;
