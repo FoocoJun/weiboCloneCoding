6efa0ed0e3a6cff9442a1d9d54014dec6dc931e7
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-07-29 17:23:48 +0900

    Setting Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import MainPage from "./components/pages/MainPage";
+import SignUpPage from "./components/pages/SignUpPage";
+import PostPage from "./components/pages/PostPage";
+import NotFoundPage from "./components/pages/NotFoundPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* 메인 페이지 */}
+        {/* 분기점으로서의 App.js // 0. 자기 자신은 index로 path 표현 가능 */}
+        {/* 와일드카드 *를 이용해 다양한 페이지 작성 가능 */}
+        <Route path="/*" element={<MainPage />} />
+        {/* 회원가입 페이지 */}
+        <Route path="/signup" element={<SignUpPage />} />
+        {/* 포스팅 페이지 */}
+        <Route path="/post" element={<PostPage />} />
+
+        {/* 잘못 된 접근 페이지 */}
+        {/* 위의 경로를 제외한 모든 주소를 NotFound로 제공 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
